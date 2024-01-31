ff41262effc0f8570ebb64967e84edb125593400
GAL\galha <galsherm@gmail.com>
2024-01-31 10:50:21 +0200

    Change two dicees

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Generate a random number between 1 and 6
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-// Output the random number to the console
-console.log(randomNumber1);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// Determine the file name of the dice image based on randomNumber1
+var diceImageName1 = "dice" + randomNumber1 + ".png";
+var diceImageName2 = "dice" + randomNumber2 + ".png";
+
+// Update the src attribute of the first <img> element with the randomly selected image
+document.querySelector(".img1").src = "images/" + diceImageName1;
+document.querySelector(".img2").src = "images/" + diceImageName2;
+
+console.log("diceImageName1 :"+ diceImageName1);
+console.log("diceImageName2 :"+ diceImageName2);
